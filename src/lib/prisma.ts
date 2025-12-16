93341ae32f766253e5acf35840d15bb36465138b
Hari Kiran Adangi <harikiran.adangi@gmail.com>
2025-12-16 22:22:42 +0530

    created new test data

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -8,7 +8,11 @@ import { Pool } from "pg";
 
 const pool = new Pool({
   connectionString: process.env.DATABASE_URL,
+  ssl: {
+    rejectUnauthorized: false,
+  },
 });
+
 
 const adapter = new PrismaPg(pool);
 
